27ceabbbefea9bf0c38a8f0f0b9f715f747a11bf
Manono <manonospaces@gmail.com>
2023-03-28 01:07:57 +0300

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void prints_rev(char *s);
+void reset_to_98(int *n);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void prints_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+#include <stdio.h>
+char *_strcpy(char *dest, char *src);
+
+#endif
